9e836d6aaa6d20969fad5ee1ff259fdf6e3c9d8e
Petrik Diak <diak@petrik.hu>
2025-12-05 14:44:16 +0100

    kesz

--- DolgozatProjekt/Dolgozat.cs
+++ DolgozatProjekt/Dolgozat.cs
@@ -20,11 +20,112 @@
 			pontok.Add(x);
 		}
 
-		public void MindenkiMegirta()
+		public bool MindenkiMegirta()
 		{
-			if (pontok.Count > 0) 
+			if (pontok.Count == 0)
 			{
-
+				throw new SenkiSemIrtException("Senki sem írt dolgozatot");
 			}
+			else if (pontok.Contains(-1))
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public int Bukas()
+		{
+			int egyes = 0;
+			foreach (var item in pontok)
+			{
+				if (item < 50 && item != -1)
+				{
+					egyes -= -1;
+				}
+			}
+			return egyes;
+		}
+
+		public int Elegseges()
+		{
+			int kettes = 0;
+			foreach (var item in pontok)
+			{
+				if (item >= 50 && item <= 60)
+				{
+					kettes -= -1;
+				}
+			}
+			return kettes;
+		}
+
+		public int Kozepes()
+		{
+			int harmas = 0;
+			foreach (var item in pontok)
+			{
+				if (item >= 61 && item <= 70)
+				{
+					harmas -= -1;
+				}
+			}
+			return harmas;
+		}
+
+		public int Jo()
+		{
+			int negyes = 0;
+			foreach (var item in pontok)
+			{
+				if (item >= 71 && item <= 80)
+				{
+					negyes -= -1;
+				}
+			}
+			return negyes;
+		}
+
+		public int Jeles()
+		{
+			int otos = 0;
+			foreach (var item in pontok)
+			{
+				if (item > 80)
+				{
+					otos -= -1;
+				}
+			}
+			return otos;
+		}
+
+		public bool Gyanus(int kivalok)
+		{
+			if (kivalok < 0)
+			{
+				throw new ArgumentException("A kiválók száma nem lehet negatív");
+			}
+			if (kivalok < Jeles())
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public bool Ervenytelen()
+		{
+			int nemIrt = 0;
+			foreach (var item in pontok)
+			{
+				if (item == -1)
+				{
+					nemIrt -= -1;
+				}
+			}
+			if (nemIrt >= pontok.Count / 2)
+			{
+				return true;
+			}
+			return false;
+		}
 	}
 }
--- DolgozatProjekt/Tests/DolgozatTest.cs
+++ DolgozatProjekt/Tests/DolgozatTest.cs
@@ -41,5 +41,248 @@ namespace DolgozatProjekt.Tests
 			Assert.That(dolgozat.Pontok[0] == -1 && dolgozat.Pontok[1] == 100);
 		}
 
+		[Test]
+		public void MindenkiMegIrtaHibas()
+		{
+			Assert.Throws<SenkiSemIrtException>(() =>
+			{
+				dolgozat.MindenkiMegirta();
+			});
+		}
+
+		[Test]
+		public void MinednkiMegIrtaIgen()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(0);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(74);
+			dolgozat.PontFelvesz(67);
+
+			Assert.That(dolgozat.MindenkiMegirta());
+		}
+
+		[Test]
+		public void MindenkiMegIrtaNem()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(74);
+			dolgozat.PontFelvesz(67);
+
+			Assert.That(!dolgozat.MindenkiMegirta());
+		}
+
+		[Test]
+		public void BukasVan()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Bukas() == 1);
+		}
+
+		[Test]
+		public void BukasNincs()
+		{
+			dolgozat.PontFelvesz(53);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Bukas() == 0);
+		}
+
+		[Test]
+		public void BukasHatar()
+		{
+			dolgozat.PontFelvesz(50);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(49);
+			dolgozat.PontFelvesz(0);
+			
+
+			Assert.That(dolgozat.Bukas() == 2);
+		}
+
+		[Test]
+		public void ElegsegesVan()
+		{
+			dolgozat.PontFelvesz(53);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Elegseges() == 1);
+		}
+
+		[Test]
+		public void ElegsegesNincs()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Elegseges() == 0);
+		}
+
+		[Test]
+		public void ElegsegesHatar()
+		{
+			dolgozat.PontFelvesz(50);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(60);
+			dolgozat.PontFelvesz(61);
+
+
+			Assert.That(dolgozat.Elegseges() == 2);
+		}
+
+		[Test]
+		public void KozepesVan()
+		{
+			dolgozat.PontFelvesz(63);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Kozepes() == 1);
+		}
+
+		[Test]
+		public void KozepesNincs()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Kozepes() == 0);
+		}
+
+		[Test]
+		public void KozepesHatar()
+		{
+			dolgozat.PontFelvesz(61);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(70);
+			dolgozat.PontFelvesz(71);
+
+
+			Assert.That(dolgozat.Kozepes() == 2);
+		}
+
+		[Test]
+		public void JoVan()
+		{
+			dolgozat.PontFelvesz(73);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Jo() == 1);
+		}
+
+		[Test]
+		public void JoNincs()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Jo() == 0);
+		}
+
+		[Test]
+		public void JoHatar()
+		{
+			dolgozat.PontFelvesz(71);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(80);
+			dolgozat.PontFelvesz(81);
+
+
+			Assert.That(dolgozat.Jo() == 2);
+		}
+
+		[Test]
+		public void JelesVan()
+		{
+			dolgozat.PontFelvesz(83);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Jeles() == 1);
+		}
+
+		[Test]
+		public void JelesNincs()
+		{
+			dolgozat.PontFelvesz(13);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Jeles() == 0);
+		}
+
+		[Test]
+		public void JelesHatar()
+		{
+			dolgozat.PontFelvesz(81);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(80);
+			dolgozat.PontFelvesz(100);
+
+
+			Assert.That(dolgozat.Jeles() == 2);
+		}
+
+		[Test]
+		public void GyanusHibas()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				dolgozat.Gyanus(-13);
+			});
+		}
+
+		[Test]
+		public void GyanusIgen()
+		{
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+
+			Assert.That(dolgozat.Gyanus(1));
+		}
+
+		[Test]
+		public void GyanusNem()
+		{
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+
+			Assert.That(!dolgozat.Gyanus(4));
+		}
+
+		[Test]
+		public void ErvenytelenIgen()
+		{
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Ervenytelen());
+		}
+
+		[Test]
+		public void ErvenytelenHatar()
+		{
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(dolgozat.Ervenytelen());
+		}
+
+		[Test]
+		public void ErvenytelenNem()
+		{
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(100);
+			dolgozat.PontFelvesz(-1);
+
+			Assert.That(!dolgozat.Ervenytelen());
+		}
 	}
 }
