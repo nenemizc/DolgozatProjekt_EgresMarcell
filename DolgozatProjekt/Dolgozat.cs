4a8c5f6b7f2e962d2d16b3ba006ce231b7eebe80
Petrik Diak <diak@petrik.hu>
2025-12-05 13:33:46 +0100

    alakul

--- DolgozatProjekt/Dolgozat.cs
+++ DolgozatProjekt/Dolgozat.cs
@@ -2,6 +2,29 @@
 {
     public class Dolgozat
     {
+		private List<int> pontok;
 
-    }
+		public Dolgozat()
+		{
+			this.pontok = new List<int>();
+		}
+
+		internal List<int> Pontok { get => pontok; }
+
+		public void PontFelvesz(int x)
+		{
+			if(x < -1 || x > 100)
+			{
+				throw new ArgumentException("Hibás pontszám.");
+			}
+			pontok.Add(x);
+		}
+
+		public void MindenkiMegirta()
+		{
+			if (pontok.Count > 0) 
+			{
+
+			}
+	}
 }
---
+++ DolgozatProjekt/Tests/DolgozatTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozatProjekt.Tests
+{
+	[TestFixture]
+	internal class DolgozatTest
+	{
+		Dolgozat dolgozat;
+
+		[SetUp]
+		public void SetUp()
+		{
+			dolgozat = new Dolgozat();
+		}
+		[Test]
+		public void PontFelveszHibas()
+		{
+			Assert.Throws<ArgumentException>(() =>
+			{
+				dolgozat.PontFelvesz(-13);
+			});
+		}
+
+		[Test]
+		public void PontFelveszJo()
+		{
+			dolgozat.PontFelvesz(13);
+			Assert.That(dolgozat.Pontok[0] == 13);
+		}
+
+		[Test]
+		public void PontFelveszHatar()
+		{
+			dolgozat.PontFelvesz(-1);
+			dolgozat.PontFelvesz(100);
+			Assert.That(dolgozat.Pontok[0] == -1 && dolgozat.Pontok[1] == 100);
+		}
+
+	}
+}
